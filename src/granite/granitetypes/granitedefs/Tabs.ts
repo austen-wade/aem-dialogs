2a9b8d29e463a3a5e2b348f41bb99c6690660526
Austen Wade <austenwade@pop-os.localdomain>
2022-10-20 20:15:22 -0600

    some moves

--- src/granite/granitetypes/granitedefs/Tabs.ts
+++ src/granite/granitetypes/granitedefs/Tabs.ts
@@ -7,28 +7,20 @@ import { Orientation, TabsSize } from "./LiteralTypes";
 export const resourceType = " /libs/granite/ui/components/coral/foundation/tabs";
 export interface Tabs extends CommonAttributes, RenderCondition, GraniteContainer {
     orientation?: Orientation;
-
     size?: TabsSize;
-
     margin?: boolean;
-
     maximized?: boolean;
-
     trackingFeature?: string;
-
     trackingWidgetName?: string;
 }
 
 export interface TabsItem {
     "jcr:title": string;
-
     trackingElement?: string;
-
     parentConfig?: GraniteNode | GraniteNode[];
 }
 
 export interface TabsItemParentconfig {
     active?: boolean;
-
     icon?: string;
 }
--- src/granite/granitetypes/granitedefs/renderconditions/Privilege.ts
+++ src/granite/granitetypes/granitedefs/renderconditions/Privilege.ts
@@ -1,6 +1,5 @@
 export const resourceType = " /libs/granite/ui/components/coral/foundation/renderconditions/privilege";
 export interface RenderConditionsPrivilege {
     path?: string;
-
     privileges?: string[];
 }
